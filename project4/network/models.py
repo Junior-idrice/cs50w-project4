e7ceccd719d699c95c0371526e402a4d7bc469c1
idrice junior <dongmojunior18@gmail.com>
2025-07-11 12:04:25 -0400

    models

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Posts(models.Model):
+    content = models.CharField(max_length=164)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"Post {self.id} by {self.user} on {self.date}"
